d781dcb726751032091186eab05cfa94c1132209
DoniRmdhn <103444731+DoniRmdhn@users.noreply.github.com>
2022-04-14 02:33:34 +0700

    Update play.py

--- YukkiMusic/utils/inline/play.py
+++ YukkiMusic/utils/inline/play.py
@@ -10,7 +10,7 @@
 from typing import Union
 
 from pyrogram.types import InlineKeyboardButton
-
+from YukkiMusic.plugins.tools.queue import queue_markup
 
 def track_markup(_, videoid, user_id, channel, fplay):
     buttons = [
